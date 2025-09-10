92a20de4966145b270b7c7ac26f5497b39c15065
develop-4 <develop-4@hornet-v.com>
2025-09-10 18:30:08 +0900

    メニューの設定削除と畳んだ時のロゴ非表示

--- src/components/molecules/SidebarHeader.tsx
+++ src/components/molecules/SidebarHeader.tsx
@@ -2,7 +2,15 @@ import Logo from '../atoms/Logo';
 
 interface SidebarHeaderProps {}
 
-export default function SidebarHeader({}: SidebarHeaderProps) {
+interface SidebarHeaderProps {
+  isCollapsed: boolean;
+}
+
+export default function SidebarHeader({ isCollapsed }: SidebarHeaderProps) {
+  if (isCollapsed) {
+    return null;
+  }
+
   return (
     <div className="flex items-center p-4 border-b border-gray-200">
       <Logo size="md" />
