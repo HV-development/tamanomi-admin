4f91b7e1612c5b17bbb16afd9b27b5ebcbeb9b7a
develop-4 <develop-4@hornet-v.com>
2025-09-10 19:02:18 +0900

    Updated DashboardLayout.tsx

--- src/components/templates/DashboardLayout.tsx
+++ src/components/templates/DashboardLayout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from '../organisms/Sidebar';
+import UserHeader from '../molecules/UserHeader';
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -8,9 +9,12 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="flex h-screen bg-gray-50">
       <Sidebar />
-      <main className="flex-1 overflow-auto">
-        <div className="p-6">
-          {children}
+      <main className="flex-1 flex flex-col overflow-hidden">
+        <UserHeader />
+        <div className="flex-1 overflow-auto">
+          <div className="p-6">
+            {children}
+          </div>
         </div>
       </main>
     </div>
